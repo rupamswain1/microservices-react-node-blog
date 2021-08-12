d30b711df9dec6ba3b4222266b6ed5640f872d2d
rupamswain1 <rupamswain1@gmail.com>
2021-08-13 00:59:21 +0530

    sercive yaml created and mongo url changed

--- node-post-service/app.js
+++ node-post-service/app.js
@@ -18,7 +18,7 @@ app.use((req,res,next)=>{
 app.use('/posts',postRoutes);
 
 
-mongoose.connect('mongodb+srv://rupam123:rupam123@nodecluster.plaky.mongodb.net/MicroserviceBlogBD?retryWrites=true&w=majority')
+mongoose.connect('mongodb://rupam123:rupam123@nodecluster-shard-00-00.plaky.mongodb.net:27017,nodecluster-shard-00-01.plaky.mongodb.net:27017,nodecluster-shard-00-02.plaky.mongodb.net:27017/MicroserviceBlogBD?ssl=true&replicaSet=atlas-t1w1wl-shard-0&authSource=admin&retryWrites=true&w=majority')
 .then(result=>{
     app.listen(8000,()=>{
         console.log('server started on 8000');
